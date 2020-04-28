502e396a89815b12292cdb777de727f03aa9cbe0
RooieDirk <DirkSmits@zonnet.nl>
2020-04-28 21:26:50 +0200

    Update radar_pi files 6

--- src/version.h
+++ src/version.h
@@ -2,5 +2,5 @@
 #define PLUGIN_VERSION_MINOR 2
 #define PLUGIN_VERSION_PATCH "1"
 #define PLUGIN_VERSION_DATE "26-4-2020"
-#define PLUGIN_VERSION "radar_pi v1.2.1-ov50"
-#define PLUGIN_VERSION_WITH_DATE "radar_pi v1.2.1-ov50 released on 26-4-2020"
+#define PLUGIN_VERSION "nmeaconverter_pi v1.2.1-ov50"
+#define PLUGIN_VERSION_WITH_DATE "nmeaconverter_pi v1.2.1-ov50 released on 26-4-2020"
